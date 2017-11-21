7bdd97f1887957a3f270fa980a54314193486d4d
capetoi1231997 <capetoi1231997@gmail.com>
2017-11-21 10:11:16 +0700

    update fontend

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const exphbs = require('express-handlebars');
 const questionRouter = require('./routers/questionRouter');
+const questionViewRouter = require('./routers/questionViewRouter');
 const userRouter = require('./routers/userRouter');
 let app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 app.use(bodyParser.urlencoded({ extended : true }) );
 app.use(bodyParser.json({ extended: true }) );
-app.use(express.static(__dirname + "/public"));
+
+app.use(express.static(__dirname + "/publics"));
 
 app.use('/api/question', questionRouter);
 app.use('/api/user',userRouter);
+app.use('/question', questionViewRouter);
 
 mongoose.connect("mongodb://localhost/hoctienganh", (err) => {
   if (err) {
